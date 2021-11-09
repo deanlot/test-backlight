6401195a5ffc15175bb4566c2d87614cd990b784
Dean <6858774+undefined@users.noreply.github.com>
2021-11-09 23:03:06 +0000

    Push from add-shit

---
+++ rad-button/stories/RadButton.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import RadButton from '../src/RadButton';
+
+export const secondary_disabled = () => (
+  <>
+    <RadButton variant={'secondary'}>Lekker</RadButton>
+    <RadButton variant={'primary'}>PRIMARY</RadButton>
+  </>
+);
