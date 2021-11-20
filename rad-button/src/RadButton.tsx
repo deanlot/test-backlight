5a16b9be8ef98972363a34865bcc862c44b4a219
Ram Alvapillai <ram.alvapillai@bench.co>
2021-11-19 16:43:13 -0800

    button-pairing: implemented done functionality

--- rad-button/src/RadButton.tsx
+++ rad-button/src/RadButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ButtonHTMLAttributes, ReactNode, useEffect, useState } from 'react';
+import { ButtonHTMLAttributes, ReactNode, useEffect, useState, MouseEvent } from 'react';
 import { Button, FlexContainer, IconContainer } from './RadButton.styles';
 import spinners from 'react-spinners';
 
@@ -7,43 +7,83 @@ export type Variant = 'primary' | 'secondary' | 'ghost';
 
 export type IconPlacement = 'left' | 'right';
 
-export type ButtonProps = Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'style'> & {
+export type ButtonProps = Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'style' | 'onClick'> & {
   variant: Variant;
   icon?: ReactNode;
   iconPlacement?: IconPlacement;
   busy?: boolean;
+  notifySuccess?: boolean;
+  notifyFailure?: boolean;
+  onClick?: (e: MouseEvent<HTMLButtonElement>) => Promise<boolean>;
 };
 
 const RadButton = ({
   children,
   variant = 'secondary',
   iconPlacement = 'right',
-  disabled,
+  disabled = false,
   busy = false,
+  notifySuccess = false,
+  notifyFailure = false,
   onClick,
   ...rest
 }: ButtonProps) => {
   const [icon, setIcon] = useState<ReactNode>(rest.icon);
+  const [done, setDone] = useState<boolean>(false);
 
-  useEffect(() => {
+  const setIcon2 = () => {
     if (!disabled && busy) {
       setIcon(<spinners.ClipLoader color="#2D3036" size={'18px'} />);
     } else {
       setIcon(rest.icon);
     }
-  }, [busy]);
+  };
+
+  useEffect(() => {
+    setIcon2();
+  }, [busy, disabled, rest.icon]);
+
+  const handleNotification = (result: boolean) => {
+    const resultingIcon = result ? <div>done</div> : <div>woops</div>;
+    setIcon(resultingIcon);
+    setDone(true);
+    setTimeout(() => {
+      setDone(false);
+      setIcon2();
+    }, 1200);
+  };
+
+  const handleClick = (e) =>
+    onClick &&
+    !done &&
+    onClick(e)
+      // show success icon for 1200 ms
+      .then((res) => {
+        if (notifySuccess) {
+          handleNotification(true);
+        }
+      })
+      // animate with shudder or something
+      .catch((e) => {
+        if (notifyFailure) {
+          handleNotification(false);
+        }
+      });
 
   return (
-    <Button.Comp
-      className={Button.styles({ variant, clickable: !busy })}
-      disabled={disabled}
-      onClick={!busy && onClick}
-    >
-      <FlexContainer.Comp className={FlexContainer.styles({ iconPlacement })}>
-        <span>{children}dsdsdsdsdd</span>
-        {icon && <IconContainer>{icon}</IconContainer>}
-      </FlexContainer.Comp>
-    </Button.Comp>
+    <div>
+      <Button.Comp
+        className={Button.styles({ variant, clickable: !busy || !done })}
+        disabled={disabled}
+        onClick={!busy && handleClick}
+        role="button"
+      >
+        <FlexContainer.Comp className={FlexContainer.styles({ iconPlacement })}>
+          <span>{children}dsdsdsdsdd</span>
+          {icon && <IconContainer>{icon}</IconContainer>}
+        </FlexContainer.Comp>
+      </Button.Comp>
+    </div>
   );
 };
 
--- rad-button/stories/RadButton.stories.tsx
+++ rad-button/stories/RadButton.stories.tsx
@@ -3,23 +3,70 @@ import RadButton from '../src/RadButton';
 import { layout } from '../../rad-utils/src/story-decorator';
 export default layout;
 
-export const primaryReady = () => <RadButton variant={'primary'}>PRIMARY</RadButton>;
+export const primaryReady = () => (
+  <RadButton
+    variant={'primary'}
+    notifySuccess
+    onClick={() =>
+      new Promise<boolean>((resolve, reject) => {
+        return resolve(true);
+      })
+    }
+  >
+    PRIMARY
+  </RadButton>
+);
 
-export const primaryBusy = () => <RadButton variant={'primary'} busy>PRIMARY</RadButton>;
+export const primaryBusy = () => (
+  <RadButton variant={'primary'} busy>
+    PRIMARY
+  </RadButton>
+);
 
-export const primaryDisabled = () => <RadButton variant={'primary'} disabled>PRIMARY</RadButton>;
+export const primaryDisabled = () => (
+  <RadButton variant={'primary'} disabled>
+    PRIMARY
+  </RadButton>
+);
 
-export const secondaryReady = () => <RadButton variant={'secondary'}>SECONDARY</RadButton>;
+export const secondaryReady = () => (
+  <RadButton
+    variant={'secondary'}
+    notifyFailure
+    onClick={() =>
+      new Promise<boolean>((resolve, reject) => {
+        return reject(true);
+      })
+    }
+  >
+    SECONDARY
+  </RadButton>
+);
 
-export const secondaryBusy = () => <RadButton variant={'secondary'} busy>SECONDARY</RadButton>;
+export const secondaryBusy = () => (
+  <RadButton variant={'secondary'} busy>
+    SECONDARY
+  </RadButton>
+);
 
-export const secondaryDisabled = () => <RadButton variant={'secondary'} disabled>SECONDARY</RadButton>;
+export const secondaryDisabled = () => (
+  <RadButton variant={'secondary'} disabled>
+    SECONDARY
+  </RadButton>
+);
 
 export const ghostReady = () => <RadButton variant={'ghost'}>SECONDARY</RadButton>;
 
-export const ghostBusy = () => <RadButton variant={'ghost'} busy>SECONDARY</RadButton>;
+export const ghostBusy = () => (
+  <RadButton variant={'ghost'} busy>
+    SECONDARY
+  </RadButton>
+);
 
-export const ghostDisabled = () => <RadButton variant={'ghost'} disabled>SECONDARY</RadButton>;
-
+export const ghostDisabled = () => (
+  <RadButton variant={'ghost'} disabled>
+    SECONDARY
+  </RadButton>
+);
 
 // export const primaryWithIcon = () => <RadButton variant={'primary'} icon={<div>testCOmponent</div>}>PRIMARY ICON</RadButton>;
