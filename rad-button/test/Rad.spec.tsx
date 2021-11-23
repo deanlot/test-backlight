bd0ba515415acd02c4b13b6618d61b91b28f6b6b
Ram Alvapillai <ram.alvapillai@bench.co>
2021-11-23 13:18:00 -0800

    button-pairing: fixed tests

--- rad-button/test/Rad.spec.tsx
+++ rad-button/test/Rad.spec.tsx
@@ -5,22 +5,29 @@ import RadButton from '../src/RadButton';
 
 const expect = chai.expect;
 
-describe('RadButton', function() {
-
-  it('should render the component', function() {
+describe('RadButton', function () {
+  it('should render the component', function () {
     const { getByText } = render(<RadButton variant={'primary'}>primary</RadButton>);
     expect(getByText('primary')).to.exist;
   });
 
   it('should present user with done icon when the button is clicked and notifySuccess is true', async () => {
-    const { getByText, getByTestId, findByText } = render(<RadButton data-testid={'save-button'} variant={'primary'}
-                                                                     notifySuccess={true} onClick={() =>
-      new Promise<void>((resolve) => {
-        return setTimeout(() => {
-          return resolve();
-        }, 10);
-      })
-    }>primary</RadButton>);
+    const { getByText, getByTestId, findByText } = render(
+      <RadButton
+        data-testid={'save-button'}
+        variant={'primary'}
+        showOnClickResult={true}
+        onClick={() =>
+          new Promise<void>((resolve) => {
+            return setTimeout(() => {
+              return resolve();
+            }, 10);
+          })
+        }
+      >
+        primary
+      </RadButton>
+    );
     expect(getByText('primary')).to.exist;
     expect(getByTestId('save-button')).to.exist;
     fireEvent.click(getByTestId('save-button'));
@@ -31,15 +38,23 @@ describe('RadButton', function() {
 
   it('should not allow a user to click while promise is still pending', async () => {
     const clickArr = [];
-    const { getByText, getByTestId, findByText } = render(<RadButton data-testid={'save-button'} variant={'primary'}
-                                                                     notifySuccess={true} onClick={() =>
-      new Promise<void>((resolve) => {
-        return setTimeout(() => {
-          clickArr.push(1);
-          return resolve();
-        }, 10);
-      })
-    }>primary</RadButton>);
+    const { getByText, getByTestId, findByText } = render(
+      <RadButton
+        data-testid={'save-button'}
+        variant={'primary'}
+        showOnClickResult={true}
+        onClick={() =>
+          new Promise<void>((resolve) => {
+            return setTimeout(() => {
+              clickArr.push(1);
+              return resolve();
+            }, 10);
+          })
+        }
+      >
+        primary
+      </RadButton>
+    );
     expect(getByText('primary')).to.exist;
     expect(getByTestId('save-button')).to.exist;
     await fireEvent.click(getByTestId('save-button'));
@@ -51,15 +66,23 @@ describe('RadButton', function() {
 
   it('should show  failure state when a sync onClick fails', async () => {
     const clickArr = [];
-    const { getByText, getByTestId, findByText } = render(<RadButton data-testid={'save-button'} variant={'primary'}
-                                                                     notifySuccess={true} onClick={() =>
-      new Promise<void>((resolve) => {
-        return setTimeout(() => {
-          clickArr.push(1);
-          return resolve();
-        }, 10);
-      })
-    }>primary</RadButton>);
+    const { getByText, getByTestId, findByText } = render(
+      <RadButton
+        data-testid={'save-button'}
+        variant={'primary'}
+        showOnClickResult={true}
+        onClick={() =>
+          new Promise<void>((resolve) => {
+            return setTimeout(() => {
+              clickArr.push(1);
+              return resolve();
+            }, 10);
+          })
+        }
+      >
+        primary
+      </RadButton>
+    );
     expect(getByText('primary')).to.exist;
     expect(getByTestId('save-button')).to.exist;
     await fireEvent.click(getByTestId('save-button'));
