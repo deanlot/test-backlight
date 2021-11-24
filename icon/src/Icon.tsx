6fb8555f125cc6dfa0bcacedd4b13f98e1d04d6f
Ram Alvapillai <ram.alvapillai@bench.co>
2021-11-24 14:58:45 -0800

    icons: added basic icon doc structure

--- icon/src/Icon.tsx
+++ icon/src/Icon.tsx
@@ -2,15 +2,9 @@ import React from 'react';
 import CustomIcon from '../../custom-icon/src/CustomIcon';
 import { icons, PickedSVGProps } from '../../icons/icons';
 
-interface IconProps extends PickedSVGProps {
-  iconName: keyof typeof icons;
-  size?: string;
-  fill?: string;
-  strokeLineJoin?: string;
-  strokeWidth?: string;
-  transform?: string;
-}
-
+/**
+ Icon component that renders SVGs from our icon library
+ */
 const Icon = ({ iconName, ...props }: IconProps) => {
   const { paths, size, fill, stroke, strokeLinecap, strokeLinejoin, strokeWidth, transform } = icons[iconName];
   const svgSize = props.size || size;
@@ -34,5 +28,10 @@ const Icon = ({ iconName, ...props }: IconProps) => {
 
   return <CustomIcon label={iconName} svg={SVG} {...props} />;
 };
+
+interface IconProps extends PickedSVGProps {
+  iconName: keyof typeof icons;
+  size?: string;
+}
 
 export default Icon;
