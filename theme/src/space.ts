b0a65f01b132f6a41790057c9514bdebb97967cf
Jason Saelhof <jason.saelhof@bench.co>
2021-11-24 08:41:55 -0700

    Update scale with 12px spacing

--- theme/src/space.ts
+++ theme/src/space.ts
@@ -2,9 +2,10 @@ export const space = {
     0: '0rem',    // 0px
     1: '0.25rem', // 4px
     2: '0.5rem',  // 8px
-    3: '1rem',    // 16px
-    4: '1.5rem',  // 24px
-    5: '2rem',    // 32px
-    6: '2.5rem',  // 40px
-    7: '3rem',    // 56px
+    3: '0.75rem', // 12px
+    4: '1rem',    // 16px
+    5: '1.5rem',  // 24px
+    6: '2rem',    // 32px
+    7: '2.5rem',  // 40px
+    8: '3rem',    // 56px
 };
