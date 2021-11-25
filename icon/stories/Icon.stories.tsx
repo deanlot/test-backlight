a1651c8207525f04e2fd3ce6abdec1f97ab532eb
Ram Alvapillai <ram.alvapillai@bench.co>
2021-11-24 17:14:16 -0800

    icons: added a few icon stories

--- icon/stories/Icon.stories.tsx
+++ icon/stories/Icon.stories.tsx
@@ -3,4 +3,7 @@ import Icon from '../src/Icon';
 import { layout } from '../../rad-utils/src/story-decorator';
 export default layout;
 
-export const IconOne = () => <Icon icon="home" size="s" />;
+export const IconSmall = () => <Icon icon="home" size="s" />;
+export const IconMedium = () => <Icon icon="home" size="m" />;
+export const IconLarge = () => <Icon icon="home" size="l" />;
+export const IconExtraLarge = () => <Icon icon="home" size="xl" />;
