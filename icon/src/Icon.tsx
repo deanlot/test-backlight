8f55f2a3712573c91aead3202fbec4b3c161d47a
Ram Alvapillai <ram.alvapillai@bench.co>
2021-11-26 17:08:52 -0800

    icon-alternative: added comments and removed old code

--- icon/src/Icon.tsx
+++ icon/src/Icon.tsx
@@ -9,9 +9,12 @@ const Icon = ({ label, color, svg }: IconProps) => (
   <CustomIcon
     label={label}
     svg={
+      // this styled component just removes lineheight to ensure the wrapping div does not affect spacing at all
+      // it also allows us access to the css prop over the usual react inline style prop
       <SVGStyleWrapper
         css={{
           [`& svg > path`]: {
+            // @Jason be proud of me, I did the thing!!
             ...(color && { stroke: color }),
           },
         }}
--- icon/src/utils.ts
+++
@@ -1,4 +0,0 @@
-export const getScale = (selectedSize, width, height) => ({
-  width: selectedSize / width,
-  height: selectedSize / height,
-});
