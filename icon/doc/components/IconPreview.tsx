f96ec59a000c695b0d580e80920fc84407c361fb
Ram Alvapillai <ram.alvapillai@bench.co>
2021-11-30 14:07:22 -0800

    icon-previous: added more icons and finished off the docs

--- icon/doc/components/IconPreview.tsx
+++ icon/doc/components/IconPreview.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { GroupContainer, IconContainer, IconName } from './IconPreview.styles';
 import CheckIcon from '../../icons/CheckIcon/src/CheckIcon';
 import BenchIcon from '../../icons/BenchIcon/src/BenchIcon';
+import ArrowRightIcon from '../../icons/ArrowRightIcon/src/ArrowRightIcon';
+import CloseIcon from '../../icons/CloseIcon/src/CloseIcon';
 
 const IconPreview = () => {
   return (
@@ -13,6 +15,14 @@ const IconPreview = () => {
       <IconContainer>
         <BenchIcon />
         <IconName>BenchIcon</IconName>
+      </IconContainer>
+      <IconContainer>
+        <ArrowRightIcon />
+        <IconName>ArrowRightIcon</IconName>
+      </IconContainer>
+      <IconContainer>
+        <CloseIcon />
+        <IconName>CloseIcon</IconName>
       </IconContainer>
     </GroupContainer>
   );
---
+++ icon/icons/ArrowRightIcon/src/ArrowRightIcon.tsx
@@ -0,0 +1,20 @@
+import React, { ReactElement } from 'react';
+import Icon from '../../../src/Icon';
+import { IconProps } from '../../types';
+
+const path = [
+  <path d="M0.375 6.29688H11.625" stroke="#121316" strokeWidth="0.75" strokeLinecap="round" strokeLinejoin="round" />,
+  <path
+    d="M6.375 11.8068L11.625 6.29695L6.375 0.787109"
+    stroke="#121316"
+    strokeWidth="0.75"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  />,
+];
+
+const ArrowRightIcon = ({ size = 'm', color, label, fill }: IconProps): ReactElement => {
+  return <Icon label={label || 'arrow-right'} size={size} path={path} color={color} fill={fill} />;
+};
+
+export default ArrowRightIcon;
