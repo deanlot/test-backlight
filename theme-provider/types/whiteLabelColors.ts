adc12c01e3b631a3ff6e24d991c8157b910685d5
Jason Saelhof <jason.saelhof@bench.co>
2021-11-30 14:32:02 -0700

    Add comments to clarify placeholder examples

--- theme-provider/types/whiteLabelColors.ts
+++ theme-provider/types/whiteLabelColors.ts
@@ -1,4 +1,6 @@
+// TODO: We need to define the actual inputs we expect a white label customer will give us.
+// These are examples only!
 export type WhiteLabelColors = {
-  background?: string;
-  text?: string;
+  background?: string; // TODO: This is an example
+  text?: string; // TODO: This is an example
 };
--- theme-provider/utils/composeTheme.ts
+++ theme-provider/utils/composeTheme.ts
@@ -3,11 +3,15 @@ import { ThemeBase } from '../../theme';
 import { ConfigType } from '@stitches/react/types/config';
 import { themeList } from '../../theme-list';
 
+// Composing a theme requires mering a customer's colors (WhiteLabelColors) over top of
+// a specific base theme (light/dark). However we expect a much smaller list of colors from the customer
+// and do not expect to give them our entire set of tokens so it isn't a simple merge. Instead
+// as we merge we will map their inputs to our tokens, comping the final theme.
 export const composeTheme = (themeBase: ThemeBase, colors: WhiteLabelColors = {}): ConfigType.Theme => ({
   ...themeList[themeBase],
   colors: {
     ...themeList[themeBase].colors,
-    ...(colors?.background && { 'surface-primary': colors.background }),
-    ...(colors?.text && { 'onSurface-borderPrimary': colors.text }),
+    ...(colors?.background && { 'surface-primary': colors.background }), // TODO: This is an example only!
+    ...(colors?.text && { 'onSurface-borderPrimary': colors.text }), // TODO: This is an example only!
   },
 });
