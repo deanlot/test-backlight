3fdd71674f1f1d4223d7330807b08abeebd2b2c0
Jason Saelhof <jason.saelhof@bench.co>
2021-12-01 08:59:45 -0700

    Fix type exports

--- theme-provider/index.ts
+++ theme-provider/index.ts
@@ -1,2 +1,3 @@
 export { ThemeProvider, useTheme } from './src/ThemeProvider';
-export { WhiteLabelColors } from './src/types/whiteLabelColors';
+export type { WhiteLabelColors } from './src/types/whiteLabelColors';
+export type { ThemeContextConfig } from './src/types/themeContextConfig';
