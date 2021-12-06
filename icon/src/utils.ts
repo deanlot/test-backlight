9b0ecb47bc68ef5ab26d24717f0441abab00d8cd
Ram Alvapillai <ram.alvapillai@bench.co>
2021-12-06 11:42:36 -0800

    text-field-div: added types to utility functions

--- icon/src/utils.ts
+++ icon/src/utils.ts
@@ -1,8 +1,13 @@
 // use the minimum scale multiplier to prevent the svg from overflowing out of the view box
-export const getScaleMultiplier = (selectedSize, width, height) =>
+export const getScaleMultiplier = (selectedSize, width, height): number =>
   Math.min(selectedSize / width, selectedSize / height);
 
-export const getCenteringTranslationValues = (size, width, height, scaleMultiplier) => ({
+type CenteringTranslationValues = {
+  translateX: number;
+  translateY: number;
+};
+
+export const getCenteringTranslationValues = (size, width, height, scaleMultiplier): CenteringTranslationValues => ({
   translateX: (size - width * scaleMultiplier) / 2,
   translateY: (size - height * scaleMultiplier) / 2,
 });
