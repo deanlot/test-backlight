be384a1f2db71ac7d8a9aee6938f18259e2b645b
Ram Alvapillai <ram.alvapillai@bench.co>
2021-12-07 11:13:54 -0800

    symbols: added flex comment

--- icon/src/Icon.styles.tsx
+++ icon/src/Icon.styles.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { styled } from '@stitches/react';
 
+/*
+ * Display flex is required for "small" sized icons specifically, to ensure the svg is centered in side of the div.
+ * Without it, the "small" sized svg will be pushed downwards outside of the div.
+ */
 export const IconContainer = styled('div', {
   display: 'flex',
 });
