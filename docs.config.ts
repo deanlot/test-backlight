ed76967ccc8a2324c07dc3dc18a54a644f687ce8
Jason Saelhof <jason.saelhof@bench.co>
2022-01-11 08:23:12 -0700

    Some work hacking at the layout to make our own menu

---
+++ docs.config.ts
@@ -0,0 +1,35 @@
+export const docsConfig = [
+  {
+    label: 'Theme',
+    items: [
+      { type: 'link', label: 'Colors', url: 'theme/doc/colors.mdx' },
+      { type: 'link', label: 'Spacing', url: 'theme/doc/space.mdx' },
+      { type: 'link', label: 'Fonts', url: 'theme/doc/fonts.mdx' },
+      { type: 'link', label: 'Font Sizes', url: 'theme/doc/fontSize.mdx' },
+      { type: 'link', label: 'Font Weights', url: 'theme/doc/fontWeight.mdx' },
+      { type: 'link', label: 'Letter Spacing', url: 'theme/doc/letterSpacing.mdx' },
+      { type: 'link', label: 'lineHeight', url: 'theme/doc/lineHeight.mdx' },
+      { type: 'link', label: 'Border Radius', url: 'theme/doc/radii.mdx' },
+      { type: 'link', label: 'Border Widths', url: 'theme/doc/borderWidths.mdx' },
+    ],
+  },
+  {
+    label: 'Components',
+    items: [
+      { type: 'link', label: 'Button', url: 'Button/doc/Button.mdx' },
+      { type: 'link', label: 'Feedback Button', url: 'FeedbackButton/doc/FeedbackButton.mdx' },
+      { type: 'link', label: 'Accessible Icon', url: 'AccessibleIcon/doc/accessibleIcon.mdx' },
+      { type: 'link', label: 'Modal', url: 'Modal/doc/Modal.mdx' },
+      { type: 'link', label: 'Text Input', url: 'TextInput/doc/TextInput.mdx' },
+      { type: 'link', label: 'Typography', url: 'Typography/doc/Typography.mdx' },
+    ],
+  },
+  {
+    label: 'Bench Design',
+    items: [
+      { type: 'link', label: 'Topic 1', url: '' },
+      { type: 'link', label: 'Topic 2', url: '' },
+      { type: 'link', label: 'Topic 3', url: '' },
+    ],
+  },
+];
--- layout/src/mdx-layout.tsx
+++ layout/src/mdx-layout.tsx
@@ -1,49 +1,148 @@
-import React from 'react';
+import React, { PropsWithChildren, ReactElement } from 'react';
 import { MDXProvider } from '@mdx-js/react';
 import { CoreLayout } from '@divriots/dockit-react/mdx-layout-core';
 import { ThemeProvider } from '../../theme-provider';
 import { BENCH_LOGO } from './bench-logo';
+import { docsConfig } from '../../docs.config';
 
 export const MdxLayout = ({ components = {}, __context, ...rest }) => (
   <ThemeProvider>
+    {console.log({ components, __context, ...rest })}
+    {console.log(docsConfig)}
     <MDXProvider components={{ ...components }}>
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css/out/light.min.css" />
-      <CoreLayout
-        // @ts-expect-error no type def for logo
-        logo={<img src={BENCH_LOGO} width="180px" />}
-        // Hack the left-menu generation.
-        // Remove the theme page and replace it with individual pages for each part of the theme.
-        __context={{
-          ...__context,
-          pages: [
-            ...__context.pages.filter(({ nav }) => nav.key !== 'theme'),
-            ...themeDocs.map((doc) => buildDocPage(doc)),
-          ],
-        }}
-        {...rest}
-      />
+
+      <PageLayout logo={BENCH_LOGO}>
+        <Menu config={docsConfig} />
+        <div>{rest.children}</div>
+      </PageLayout>
     </MDXProvider>
   </ThemeProvider>
 );
 
-const themeDocs = [
-  'colors',
-  'space',
-  'fontSize',
-  'fonts',
-  'fontWeight',
-  'lineHeight',
-  'letterSpacing',
-  'radii',
-  'borderWidths',
-];
+const relativeUrl = (url: string) =>
+  [
+    Array(url.split('/').length - 1)
+      .fill('..')
+      .join('/'),
+    url,
+  ].join('/');
 
-const buildDocPage = (id) => ({
-  url: `theme/doc/${id}`,
-  input: `theme/doc/${id}.mdx`,
-  data: {},
-  nav: {
-    key: id,
-    parent: 'Theme',
-  },
-});
+type PageLayoutProps = {
+  logo: string;
+};
+
+const PageLayout = ({ children, logo }: PropsWithChildren<PageLayoutProps>): ReactElement => (
+  <>
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: 'auto 1fr',
+        justifyItems: 'end',
+        alignItems: 'end',
+        borderBottom: '1px solid grey',
+        marginBottom: 24,
+      }}
+    >
+      <img src={logo} width="180px" />
+      <h3 style={{ marginBottom: 20 }}>Bench Design System</h3>
+    </div>
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: 'max-content 1fr',
+        columnGap: 64,
+      }}
+    >
+      {children}
+    </div>
+  </>
+);
+
+type Section = {
+  label: string;
+  items: SectionItem[];
+};
+
+type SectionItem = {
+  type: string;
+  label: string;
+  url?: string;
+};
+
+type MenuConfigProps = {
+  config: Section[];
+};
+
+const Menu = ({ config }: MenuConfigProps): ReactElement => (
+  <div>
+    {config.map(({ label, items }, index) => (
+      <MenuSection first={index === 0} key={label}>
+        <MenuSectionLabel>{label}</MenuSectionLabel>
+        <MenuSectionItems items={items} />
+      </MenuSection>
+    ))}
+  </div>
+);
+
+type MenuSectionProps = {
+  first?: boolean;
+};
+
+const MenuSection = ({ first = false, children }: PropsWithChildren<MenuSectionProps>): ReactElement => (
+  <div style={first ? { marginTop: 32 } : { borderTop: '1px solid lightgrey', paddingTop: 24, marginTop: 24 }}>
+    {children}
+  </div>
+);
+
+type MenuSectionLabelProps = {
+  children: React.ReactNode;
+};
+
+const MenuSectionLabel = ({ children }: MenuSectionLabelProps): ReactElement => (
+  <div style={{ fontWeight: 'medium' }}>{children}</div>
+);
+
+type MenuSectionItemsProps = {
+  items: SectionItem[];
+};
+
+const MenuSectionItems = ({ items }: MenuSectionItemsProps): ReactElement => {
+  const ComponentType = {
+    subsection: MenuSubsectionLabel,
+    link: MenuSubsectionItem,
+  };
+
+  return (
+    <ul style={{ listStyleType: 'none', padding: 0, margin: '16px 0 0' }}>
+      {items.map(({ type, label, ...rest }) => {
+        const Component = ComponentType[type];
+        return (
+          <Component key={`${type}-${label}`} {...rest}>
+            {label}
+          </Component>
+        );
+      })}
+    </ul>
+  );
+};
+
+type MenuSubsectionLabelProps = {
+  children: React.ReactNode;
+};
+
+const MenuSubsectionLabel = ({ children }: MenuSectionLabelProps): ReactElement => (
+  <div style={{ fontSize: 10, textTransform: 'uppercase', margin: '16px 0 8px' }}>{children}</div>
+);
+
+type MenuSubsectionItemProps = {
+  url: string;
+};
+
+const MenuSubsectionItem = ({ url, children }: PropsWithChildren<MenuSubsectionItemProps>): ReactElement => (
+  <li style={{ paddingLeft: 4 }}>
+    <a href={relativeUrl(url)} style={{ margin: '4px 0', fontSize: 14 }}>
+      {children}
+    </a>
+  </li>
+);
